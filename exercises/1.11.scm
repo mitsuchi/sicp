bdcbda75ba7d9070fbd1bf11ca7bb1971207598a
Tatsuo Mitsuchi <t.mitsuchi3@gmail.com>
2013-05-27 00:20:50 +0900

    回答を一部修正

--- exercises/1.11.scm
+++ exercises/1.11.scm
@@ -1,41 +1,45 @@
 ; まず再帰的プロセスの場合
 
 (define (f-r n)
-  (if (<= n 3)
+  (if (< n 3)
 	  n
-	  (+ (f-r (- n 1)) (f-r (- n 2)) (f-r (- n 3)))))
+	  (+ (f-r (- n 1)) (* 2 (f-r (- n 2))) (* 3 (f-r (- n 3))))))
 
+(print (f-r 0))
+; 0
 (print (f-r 1))
 ; 1
 (print (f-r 2))
 ; 2
 (print (f-r 3))
-; 3
+; 4
 (print (f-r 4))
-; 6
-(print (f-r 5))
 ; 11
+(print (f-r 5))
+; 25
 (print (f-r 6))
-; 20
+; 59
 
 ; 繰り返しプロセスの場合
 
 (define (f-i a b c count)
   (if (= count 0)
 	  a
-	  (f-i b c (+ a b c) (- count 1))))
+	  (f-i b c (+ (* 3 a) (* 2 b) c) (- count 1))))
 
 ;aがf(n), bがf(n+1), cがf(n+2)を表す。
 ;(f-i 0 1 2 5)
-;(f-i 1 2 3 4)
-;(f-i 2 3 6 3)
-;(f-i 3 6 11 2)
-;(f-i 6 11 20 1)
-;(f-i 11 20 32 0)
+;(f-i 1 2 4 4)
+;(f-i 2 4 11 3)
+;(f-i 4 11 25 2)
+;(f-i 11 25 59 1)
+;(f-i 25 59 142 0)
 
 (define (f n)
   (f-i 0 1 2 n))
 
+(print (f 0))
+; 0
 (print (f 1))
 ; 1
 (print (f 2))
--- exercises/1.3.scm
+++ exercises/1.3.scm
@@ -1,17 +1,20 @@
+(define (sum-of-squares-larger-two a b c)
+  (cond ((= (min a b c) a)
+		 (sum-of-squares b c))
+		((= (min a b c) b)
+		 (sum-of-squares a c))
+		(else
+		 (sum-of-squares a b))))
+
 (define (square a)
   (* a a))
 
-(define (sum-of-squares-larger-two a b c)
-  (cond ((= (min a b c) a)
-		 (+ (square b) (square c)))
-		((= (min a b c) b)
-		 (+ (square a) (square c)))
-		(else
-		 (+ (square a) (square b)))))
+(define (sum-of-squares a b)
+  (+ (* a a) (* b b)))
 
 ; 大きい2つ = 最小じゃないやつ。
 
-(sum-of-squares-larger-two 1 2 3)
-; gosh> 13
-(sum-of-squares-larger-two 5 4 3)
-; gosh> 41
+(print (sum-of-squares-larger-two 1 2 3))
+; 13
+(print (sum-of-squares-larger-two 5 4 3))
+; 41
