27a89c1b570586adc9e91632233cb86f21ce4b83
Tatsuo Mitsuchi <t.mitsuchi3@gmail.com>
2013-07-12 08:19:53 +0900

    2.17をnullに対応

--- exercises/2.17.scm
+++ exercises/2.17.scm
@@ -1,11 +1,13 @@
 ; リストの最後の要素を返す。
 ; 方針：前から順番に見ていって、次がなければ最後
 
+(define nil (list))
+
 (define (last-pair items)
-  (if (null? (cdr items))
-      (list (car items))
-      (last-pair (cdr items))))
+  (cond ((null? items) nil)
+		((null? (cdr items)) (list (car items)))
+		(else (last-pair (cdr items)))))
 
 (print (last-pair (list 1 2 3 4 5)))
 (print (last-pair (list 1)))
-
+(print (last-pair (list)))
